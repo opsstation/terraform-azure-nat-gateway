84f6cf13ebea5b4f34603708181236340072bea2
sohanyadav <sohanyadavdevops@gmail.com>
2025-12-02 14:54:44 +0530

    initial commit

--- _example/example.tf
+++ _example/example.tf
@@ -9,14 +9,14 @@ locals {
 }
 
 module "resource_group" {
-  source      = "git::https://github.com/opsstation/terraform-azure-resource-group.git?ref=v1.0.0"
+  source      = "git::https://github.com/opsstation/terraform-azure-resource-group.git?ref=v1.0.1"
   name        = local.name
   environment = local.environment
   location    = local.location
 }
 
 module "vnet" {
-  source              = "git::https://github.com/opsstation/terraform-azure-vnet.git?ref=v1.0.0"
+  source              = "git::https://github.com/opsstation/terraform-azure-vnet.git?ref=v1.0.1"
   name                = local.name
   environment         = local.environment
   resource_group_name = module.resource_group.resource_group_name
@@ -25,7 +25,7 @@ module "vnet" {
 }
 
 module "subnet" {
-  source               = "git::https://github.com/opsstation/terraform-azure-subnet.git?ref=v1.0.1"
+  source               = "git::https://github.com/opsstation/terraform-azure-subnet.git?ref=v1.0.2"
   name                 = local.name
   environment          = local.environment
   resource_group_name  = module.resource_group.resource_group_name
